68b92fbb26e5f1417128691c73acf2dfac24c889
uduak1 <juduak75@gmail.com>
2023-09-19 00:31:03 +0000

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,12 @@
 #include "main.h"
+
 /**
-* reset_to_98.c - the function name
+* reset_to_98 - the function name
 * @n: function parameter
+* Return: nothing
 */
 
-void reset_to_98(int *n);
+	void reset_to_98(int *n)
 {
-
 	*n = 98;
 }
-
